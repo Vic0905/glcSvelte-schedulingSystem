12458ea19d78b02cc93aff78b778c6ef2e36924a
chaaanito <christian.carl.donor@gmail.com>
2025-07-16 13:12:02 +0800

    refactor: clean up markup in LandingPage.svelte for better readability

--- src/routes/LandingPage/LandingPage.svelte
+++ src/routes/LandingPage/LandingPage.svelte
@@ -8,49 +8,28 @@
 </script>
 
 <div class="flex items-center justify-center overflow-hidden">
-
-
   <!-- Content Layer -->
   <div class="p-8 rounded-lg text-center mt-10">
-    <h1 class="text-xl md:text-5xl font-bold tracking-wide">
-      WELCOME TO
-    </h1>
-    <h1 class="text-3xl md:text-6xl font-bold tracking-wide text-info">
-      GLOBAL LANGUAGE CEBUS
-    </h1>
+    <h1 class="text-xl md:text-5xl font-bold tracking-wide">WELCOME TO</h1>
+    <h1 class="text-3xl md:text-6xl font-bold tracking-wide text-info">GLOBAL LANGUAGE CEBU</h1>
   </div>
 </div>
 
 <div class="flex flex-row flex-wrap items-center justify-evenly gap-4 max-w-[850px] mx-auto mt-10">
   <!-- Registration -->
   <a href="/#/registration/">
-    <img src={'/register.png'}
-         alt="Registration"
-         class="w-full max-w-[400px] h-[170px] object-cover">
+    <img src={'/register.png'} alt="Registration" class="w-full max-w-[400px] h-[170px] object-cover" />
   </a>
   <!-- Request -->
   <a href="/#/request/">
-    <img src={'/request.png'}
-         alt="Registration"
-         class="w-full max-w-[400px] h-[130px] object-cover">
+    <img src={'/request.png'} alt="Registration" class="w-full max-w-[400px] h-[130px] object-cover" />
   </a>
   <!-- Rules -->
   <a href="/#/rules/">
-    <img src={'/rules.png'}
-         alt="Rules"
-         class="w-full max-w-[400px] h-[130px] object-cover">
+    <img src={'/rules.png'} alt="Rules" class="w-full max-w-[400px] h-[130px] object-cover" />
   </a>
   <!-- Electricity -->
   <a href="/#/dashboard/">
-    <img src={'/electricity.png'}
-         alt="Registration"
-         class="w-full max-w-[400px] h-[130px] object-cover">
+    <img src={'/electricity.png'} alt="Registration" class="w-full max-w-[400px] h-[130px] object-cover" />
   </a>
 </div>
-
-
-
-
-
-
-
