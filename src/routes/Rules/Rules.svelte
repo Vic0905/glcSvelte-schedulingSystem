839ecfae83215831143a8c36665a810b8b6808ba
Vic0905 <diputadojuniel@gmail.com>
2025-07-17 13:08:01 +0800

    feat: enhance registration form with additional fields and improve data handling

---
+++ src/routes/Rules/Rules.svelte
@@ -0,0 +1,63 @@
+<script>
+  import { onMount } from 'svelte'
+  import { toast } from 'svelte-sonner'
+</script>
+
+<div class="relative h-[50vh] flex items-center justify-center overflow-hidden">
+  <!-- Background Layer -->
+  <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('/bg-register.jpg');"></div>
+
+  <!-- Content Layer -->
+  <div class="relative z-10 p-8 rounded-lg text-center">
+    <h1 class="text-xl md:text-5xl font-bold tracking-wide">RULES AND REGULATIONS</h1>
+  </div>
+</div>
+
+<div class="mt-6 flex justify-center gap-10">
+  <a
+    href="/#/rules/welcome"
+    class="text-xl inline-flex items-center px-15 py-8 btn btn-accent text-white font-semibold rounded-lg shadow-md hover:btn-accent transition duration-300"
+  >
+    <!-- Scroll Icon SVG -->
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-8 h-8"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M19 17V5a2 2 0 0 0-2-2H4" />
+      <path
+        d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"
+      />
+    </svg>
+    Welcome Paper
+  </a>
+
+  <a
+    href="/#/rules/regulation"
+    class="text-xl inline-flex items-center px-15 py-8 btn btn-primary text-white font-semibold rounded-lg shadow-md hover:btn-primary transition duration-300"
+  >
+    <!-- Scale Icon SVG -->
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-10 h-10"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+      <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+      <path d="M7 21h10" />
+      <path d="M12 3v18" />
+      <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2" />
+    </svg>
+    Rules and Regulation
+  </a>
+</div>
