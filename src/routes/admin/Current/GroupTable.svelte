da490da6732aa4e36c66e97401b457e8988fcbda
Vic0905 <diputadojuniel@gmail.com>
2026-01-30 16:09:59 +0800

    style: Update table styling in GroupTable and scheduleInput components for improved layout and consistency

--- src/routes/admin/Current/GroupTable.svelte
+++ src/routes/admin/Current/GroupTable.svelte
@@ -481,9 +481,9 @@
             sort: false,
             pagination: false,
             className: {
-              table: 'w-full border text-xs',
-              th: 'bg-base-200 p-2 border text-center',
-              td: 'border p-2 align-middle text-center',
+              table: 'w-full border text-xs !border-collapse',
+              th: 'bg-base-200 p-2 border-t boder-d !border-x-0 text-center',
+              td: 'border-t border-d !border-x-0 p-2 align-middle text-center',
             },
             style: {
               table: {
